b1a943f567d44f9d38d6d8366424919a11bf36e2
SimpsonC22 <sim20012@byui.edu>
2022-04-30 18:27:10 -0600

    fixed errors

--- Program.cs
+++ Program.cs
@@ -94,7 +94,7 @@ namespace CortSimpson // Note: actual namespace depends on the project name.
         {
             bool Catgame = false;
 
-            if (board[0] != "1" && board[1] != "2" && board[2] != "3" && board[3] != "4" && board[4] != "5" && board[5] != "6" && board[6] != "7" && board[7] != "8" && board[8] != "9" && Winner == false)
+            if (board[0] != "1" && board[1] != "2" && board[2] != "3" && board[3] != "4" && board[4] != "5" && board[5] != "6" && board[6] != "7" && board[7] != "8" && board[8] != "9")
             {
                 Catgame = true;
             }
